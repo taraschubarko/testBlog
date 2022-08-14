ef8a013b96416e76b5d6b2c5626fb454a26a74ba
tarik <tchubarko@gmail.com>
2022-08-14 13:48:32 +0300

    chat

--- app/Services/ChatService.php
+++ app/Services/ChatService.php
@@ -13,22 +13,23 @@ class ChatService implements ChatServiceInterface
     {
         try {
             $me = auth()->user();
-
+            //Вибираємо повідомлення мені
             $toMe = DatabaseNotification::query()
                 ->where('type', 'App\Notifications\UserMessageNotification')
                 ->where('notifiable_id', $me->id)
                 ->where('data->sender_id', $user->id);
-
+            //Аналогічно повідомлення від мене
             $fromMe = DatabaseNotification::query()
                 ->where('type', 'App\Notifications\UserMessageNotification')
                 ->where('notifiable_id', $user->id)
                 ->where('data->sender_id', $me->id);
+            //Поєднуємо запит
             $data = $toMe->union($fromMe)->orderBy('created_at')->get();
-
+            //Відвічаємо всі повідомлення мені прочитаними, так як ми відразу будемо на сторінці чату
             $toMe->get()->each(function ($item){
                 $item->markAsRead();
             });
-
+            //Формуємо результат через ресурс
             $items = ChatResource::collection($data);
 
             return $items;
